49d2648b1641313fd2156bb3c5708a15cacef0cc
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-10-03 19:21:03 +0530

    Login and register UIs

--- app/src/main/java/com/example/simplesurveycreator/activity/Dashboard.java
+++ app/src/main/java/com/example/simplesurveycreator/activity/Dashboard.java
@@ -6,12 +6,11 @@ import android.os.Bundle;
 
 import com.example.simplesurveycreator.R;
 
-public class MainActivity extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_dashboard);
     }
 }
